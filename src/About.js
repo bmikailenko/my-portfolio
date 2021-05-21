c82b74beb5eccbdd196e10f9625790041d16b553
bmikailenko <ben.mikailenko@wsu.edu>
2021-05-21 14:02:29 -0700

    Updated About Page React Bootstrap for Mobile

--- src/About.js
+++ src/About.js
@@ -34,89 +34,125 @@ function About() {
       </div> 
 
       <div>
-        <Row className="justify-content-md-center" style={{'paddingTop' : '80px'}}>
-            <Col xs={6} md={3}>
-              <Image fluid src={vancouver} rounded />
-            </Col>
-          </Row>
-          <Row className="justify-content-md-center align-items-center">
-            <Col xs={6} md={3}>
-              {t('I was born and raised in Vancouver WA, USA.1')}
-            </Col>
-          </Row>
+        <Row className="align-items-center text-center" style={{'paddingTop' : '80px'}}>
+          <Col sm>
+            <Image fluid src={vancouver} rounded />
+          </Col>
+        </Row>
+        <Row className="align-items-center text-center">
+          <Col>
+            {t('I was born and raised in Vancouver WA, USA.1')}
+          </Col>
+        </Row>
       </div>
 
-      <div>
+        <div>
           <Row className="justify-content-md-center" style={{'paddingTop' : '80px'}}>
-            <Col md='auto'>
+            <Col md='auto' className="text-center">
               <h3>
                 {t('Education.1')}:
               </h3>   
             </Col>  
           </Row>
-          <Row className="justify-content-md-center align-items-center" style={{'paddingTop' : '40px'}}>
-            <Col md='auto'></Col>
-            <Col>
-              <Image fluid src={clark} rounded />
+
+          <Row className="align-items-center text-center">
+            <Col sm>
+              <Row md={10}>
+                <Col className="text-center">
+                  <Image fluid src={clark} rounded />
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  {t('Completed my AA degree in 2017 focused towards Business Administration at Clark College, Vancouver.1')}
+                </Col>
+              </Row>
             </Col>
-            <Col>
-              <Image fluid src={wsuv} rounded />
+            <Col sm> 
+              <Row md={10}>
+                <Col className="text-center">
+                  <Image fluid src={wsuv} rounded/>
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  {t("Completed my Bachelor's degree in Computer Science at Washington State Univeristy, Vancouver in May 2021.1")}
+                </Col>
+              </Row>
             </Col>
-            <Col>
-              <Image fluid src={wsuv2} rounded />
-            </Col>
-          </Row>
-          <Row xs={3} className="justify-content-md-center align-items-center" style={{'paddingTop' : '40px'}}>
-            <Col>
-              {t('Completed my AA degree in 2017 focused towards Business Administration at Clark College, Vancouver.1')}
-            </Col>
-            <Col>
-              {t("Completed my Bachelor's degree in Computer Science at Washington State Univeristy, Vancouver in May 2021.1")}
-            </Col>
-            <Col>
-              {t("I'm working on completing my Master's in Computer Science at Washington State University, Vancouver in May 2023.1")}
+            <Col sm>
+              <Row md={10}>
+                <Col className="text-center">
+                  <Image src={wsuv2} rounded />
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  {t("I'm working on completing my Master's in Computer Science at Washington State University, Vancouver in May 2023.1")}
+                </Col>
+              </Row>
             </Col>
           </Row>
         </div>
 
         <div>
           <Row className="justify-content-md-center" style={{'paddingTop' : '80px'}}>
-            <Col md='auto'>
+            <Col md='auto' className="text-center">
               <h3>
                 {t('Interests.1')}:
               </h3>   
             </Col>  
           </Row>
-          <Row className="justify-content-md-center align-items-center" style={{'paddingTop' : '40px'}}>
-            <Col>
-              <Image fluid src={reactImage} rounded />
-            </Col>
-            <Col>
-              <Image fluid src={c} rounded />
-            </Col>
-            <Col>
-              <Image fluid src={python} rounded />
-            </Col>
-            <Col>
-              <Image fluid src={aws} rounded />
-            </Col>
-          </Row>
-          <Row xs={4} className="justify-content-md-center align-items-center" style={{'paddingTop' : '40px', 'paddingBottom' : '80px'}}>
-            <Col>
-              {t('React JS was the reason I pursued a degree in Computer Science.1')}
-            </Col>
-            <Col>
-              {t('For my first two years I was mainly programming in C.1')}
-            </Col>
-            <Col>
-              {t('Then I mainly programmed in Python for my remaining two years.1')}
-            </Col>
-            <Col>
-              {t("When I'm not finishing assignments, I'm studying Amazon Web services.1")}
-            </Col>
-          </Row>
 
-        
+          <Row className="align-items-center text-center">
+            <Col sm>
+              <Row md={10}>
+                <Col className="text-center">
+                  <Image fluid src={reactImage} rounded />
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  {t('React JS was the reason I pursued a degree in Computer Science.1')}
+                </Col>
+              </Row>
+            </Col>
+            <Col sm> 
+              <Row md={10}>
+                <Col className="text-center">
+                  <Image fluid src={c} rounded />
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  {t('For my first two years I was mainly programming in C.1')}
+                </Col>
+              </Row>
+            </Col>
+            <Col sm>
+              <Row md={10}>
+                <Col className="text-center">
+                  <Image fluid src={python} rounded />
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  {t('Then I mainly programmed in Python for my remaining two years.1')}
+                </Col>
+              </Row>
+            </Col>
+            <Col sm>
+              <Row md={10}>
+                <Col className="text-center">
+                  <Image fluid src={aws} rounded />
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  {t("When I'm not finishing assignments, I'm studying Amazon Web services.1")}                </Col>
+              </Row>
+            </Col>
+          </Row>
       </div>
 
         <Row className="align-items-center" style={{'paddingTop' : '40px', 'textAlign' : 'center'}}>
